99170b9941d5b1a782a17e66cf4c65a458369f23
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-12-21 14:00:57 +0700

    Add project files.

---
+++ Lab02-02/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Lab02_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtAverageScore = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.optMale = new System.Windows.Forms.RadioButton();
+            this.optFemale = new System.Windows.Forms.RadioButton();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.cmbFaculty = new System.Windows.Forms.ComboBox();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.txtSumFemale = new System.Windows.Forms.TextBox();
+            this.txtSumMale = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.colStudenID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colGender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAverageScore = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFaculty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản Lý Thông Tin Sinh Viên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Thông Tin Sinh Viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mã Sinh Viên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Họ Tên";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Giới Tính";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Điểm TB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Chuyên Ngành";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(129, 116);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(128, 20);
+            this.txtStudentID.TabIndex = 1;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(129, 161);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(128, 20);
+            this.txtFullName.TabIndex = 1;
+            // 
+            // txtAverageScore
+            // 
+            this.txtAverageScore.Location = new System.Drawing.Point(129, 252);
+            this.txtAverageScore.Name = "txtAverageScore";
+            this.txtAverageScore.Size = new System.Drawing.Size(128, 20);
+            this.txtAverageScore.TabIndex = 1;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(90, 352);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Thêm/Sửa";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(182, 352);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // optMale
+            // 
+            this.optMale.AutoSize = true;
+            this.optMale.Location = new System.Drawing.Point(129, 210);
+            this.optMale.Name = "optMale";
+            this.optMale.Size = new System.Drawing.Size(47, 17);
+            this.optMale.TabIndex = 3;
+            this.optMale.TabStop = true;
+            this.optMale.Text = "Nam";
+            this.optMale.UseVisualStyleBackColor = true;
+            // 
+            // optFemale
+            // 
+            this.optFemale.AutoSize = true;
+            this.optFemale.Checked = true;
+            this.optFemale.Location = new System.Drawing.Point(191, 210);
+            this.optFemale.Name = "optFemale";
+            this.optFemale.Size = new System.Drawing.Size(39, 17);
+            this.optFemale.TabIndex = 4;
+            this.optFemale.TabStop = true;
+            this.optFemale.Text = "Nữ";
+            this.optFemale.UseVisualStyleBackColor = true;
+            // 
+            // cmbFaculty
+            // 
+            this.cmbFaculty.FormattingEnabled = true;
+            this.cmbFaculty.Items.AddRange(new object[] {
+            "QTKD",
+            "NNA",
+            "CNTT"});
+            this.cmbFaculty.Location = new System.Drawing.Point(126, 297);
+            this.cmbFaculty.Name = "cmbFaculty";
+            this.cmbFaculty.Size = new System.Drawing.Size(131, 21);
+            this.cmbFaculty.TabIndex = 5;
+            this.cmbFaculty.Text = "CNTT";
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.AllowUserToAddRows = false;
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colStudenID,
+            this.colFullName,
+            this.colGender,
+            this.colAverageScore,
+            this.colFaculty});
+            this.dgvStudent.Location = new System.Drawing.Point(277, 72);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvStudent.Size = new System.Drawing.Size(530, 278);
+            this.dgvStudent.TabIndex = 6;
+            this.dgvStudent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudent_CellClick);
+            // 
+            // txtSumFemale
+            // 
+            this.txtSumFemale.Location = new System.Drawing.Point(680, 371);
+            this.txtSumFemale.Name = "txtSumFemale";
+            this.txtSumFemale.Size = new System.Drawing.Size(58, 20);
+            this.txtSumFemale.TabIndex = 7;
+            this.txtSumFemale.Text = "0";
+            // 
+            // txtSumMale
+            // 
+            this.txtSumMale.Location = new System.Drawing.Point(580, 371);
+            this.txtSumMale.Name = "txtSumMale";
+            this.txtSumMale.Size = new System.Drawing.Size(58, 20);
+            this.txtSumMale.TabIndex = 7;
+            this.txtSumMale.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(500, 374);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Tổng SV Nam";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(653, 374);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Nữ";
+            // 
+            // colStudenID
+            // 
+            this.colStudenID.HeaderText = "MSSV";
+            this.colStudenID.Name = "colStudenID";
+            // 
+            // colFullName
+            // 
+            this.colFullName.HeaderText = "Họ Tên";
+            this.colFullName.Name = "colFullName";
+            // 
+            // colGender
+            // 
+            this.colGender.HeaderText = "Giới Tính";
+            this.colGender.Name = "colGender";
+            // 
+            // colAverageScore
+            // 
+            this.colAverageScore.HeaderText = "ĐTB";
+            this.colAverageScore.Name = "colAverageScore";
+            // 
+            // colFaculty
+            // 
+            this.colFaculty.HeaderText = "Khoa";
+            this.colFaculty.Name = "colFaculty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 517);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSumMale);
+            this.Controls.Add(this.txtSumFemale);
+            this.Controls.Add(this.dgvStudent);
+            this.Controls.Add(this.cmbFaculty);
+            this.Controls.Add(this.optFemale);
+            this.Controls.Add(this.optMale);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtAverageScore);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Lab02-02";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtAverageScore;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.RadioButton optMale;
+        private System.Windows.Forms.RadioButton optFemale;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ComboBox cmbFaculty;
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.TextBox txtSumFemale;
+        private System.Windows.Forms.TextBox txtSumMale;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStudenID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colGender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAverageScore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFaculty;
+    }
+}
+
---
+++ Lab02/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lab02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btbSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(276, 233);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(41, 38);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Location = new System.Drawing.Point(213, 120);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(53, 13);
+            this.lblNumber1.TabIndex = 1;
+            this.lblNumber1.Text = "Number 1";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(276, 117);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(239, 20);
+            this.txtNumber1.TabIndex = 2;
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Location = new System.Drawing.Point(216, 168);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(53, 13);
+            this.lblNumber2.TabIndex = 3;
+            this.lblNumber2.Text = "Number 2";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(276, 168);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(239, 20);
+            this.txtNumber2.TabIndex = 4;
+            // 
+            // btbSub
+            // 
+            this.btbSub.Location = new System.Drawing.Point(348, 233);
+            this.btbSub.Name = "btbSub";
+            this.btbSub.Size = new System.Drawing.Size(43, 38);
+            this.btbSub.TabIndex = 0;
+            this.btbSub.Text = "-";
+            this.btbSub.UseVisualStyleBackColor = true;
+            this.btbSub.Click += new System.EventHandler(this.btbSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(417, 233);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(42, 38);
+            this.btnMul.TabIndex = 0;
+            this.btnMul.Text = "x";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(483, 233);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(40, 38);
+            this.btnDiv.TabIndex = 0;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(213, 329);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(276, 326);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(247, 20);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 474);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btbSub);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Lab02-01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btbSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
